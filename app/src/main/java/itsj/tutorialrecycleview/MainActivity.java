50df20190e9509c6a97b712194cbbd6cef3c2259
Benjamin Viramontes <benja120599@gmail.com>
2019-05-17 16:48:27 -0500

    Se añadió el proyecto al repositorio

---
+++ app/src/main/java/itsj/tutorialrecycleview/MainActivity.java
@@ -0,0 +1,15 @@
+package itsj.tutorialrecycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
---
+++ app/src/main/java/itsj/tutorialrecycleview/anime_card.java
@@ -0,0 +1,13 @@
+package itsj.tutorialrecycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class anime_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.anime_card);
+    }
+}
