97a1b8520aee0f324937fb11dd9c6baac1b84aa0
Benjamin Viramontes <benja120599@gmail.com>
2019-05-17 16:58:16 -0500

    Se agregó el constructor y los métodos getters en la clase Anime

--- app/src/main/java/itsj/tutorialrecycleview/MainActivity.java
+++ app/src/main/java/itsj/tutorialrecycleview/MainActivity.java
@@ -35,5 +35,21 @@ class Anime {
     private String nombre;
     private int visitas;
 
+    public Anime(int imagen, String nombre, int visitas) {
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.visitas = visitas;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVisitas() {
+        return visitas;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
 }
