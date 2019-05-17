2f3743f14f301cd566c6889e14f97ecace69352b
Benjamin Viramontes <benja120599@gmail.com>
2019-05-17 16:54:11 -0500

    Se declara una Lista llamada items

--- app/src/main/java/itsj/tutorialrecycleview/MainActivity.java
+++ app/src/main/java/itsj/tutorialrecycleview/MainActivity.java
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicializar Animes
+        List items = new ArrayList();
+
+
     }
 
 
